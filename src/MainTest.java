321d7512d7adffc4770839afd493d50218d268f8
LPHusarz <lphusarz@gmail.com>
2018-10-17 17:46:16 +0200

    init

---
+++ src/MainTest.java
@@ -0,0 +1,10 @@
+public class MainTest
+{
+    public static void main(String[] args)
+    {
+        //W tym miejscu jedynie testujemy swoje moduły, proszę nie tworzyć modułów w tym miejscu, ani proszę nie tworzyć metod dla klasy MainTest
+
+
+    }
+
+}
