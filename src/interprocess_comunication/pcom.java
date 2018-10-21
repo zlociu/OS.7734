9bc2a1cd35fc507248e93568de1bf8cba7a673f5
Ithil Bastos <sebastian.lewandowski.bastos@gmail.com>
2018-10-21 21:41:03 +0200

    Sebastian zmienil klase Obj.java na pcom.java

---
+++ src/interprocess_comunication/pcom.java
@@ -0,0 +1,9 @@
+//Autorized by Sebastian Lewandowski in 2018
+//moduł pcom jest odpowiedzialny za komunikację międzyprocesową za pomocą komumikatów
+package interprocess_comunication;
+
+
+
+public class Obj
+{
+}
